3663183bf542e51add82cd2cca65c6df7dbeceaa
Hanif Fakira <fakira.work@gmail.com>
2016-12-03 08:32:49 +0000

    P1 changed test for different ranges

--- spec/kudomon_spec.rb
+++ spec/kudomon_spec.rb
@@ -10,16 +10,23 @@ describe Kudomon do
       expect(kudomon.type).to eq "grass"
     end
 
-    it 'has a position' do
-      allow(Kernel).to receive(:rand).and_return(4, 8)
-      expect(kudomon.position).to eq [4,8]
-    end
-
-    it 'position lies between 1 and max range' do
-      500.times do
-        expect(kudomon.position[0]).to be_between(1, 10)
-        expect(kudomon.position[1]).to be_between(1, 10)
+    describe '#position' do
+      it 'has a position' do
+        allow(Kernel).to receive(:rand).and_return(4, 8)
+        expect(kudomon.position).to eq [4,8]
       end
+
+      it 'position lies between 1 and max range' do
+        max_x_range = 5
+        max_y_range = 9
+        mancharred = Kudomon.new("Mancharred" , "fire", [max_x_range, max_y_range] )
+        500.times do
+          expect(mancharred.position[0]).to be_between(1, max_x_range)
+          expect(mancharred.position[1]).to be_between(1, max_y_range)
+        end
+      end
+
+      it 'has a default'
     end
   end
 end
