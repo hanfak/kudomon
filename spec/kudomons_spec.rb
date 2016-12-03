de08df79647c4259f0502dd250e5e8ae39399ccb
Hanif Fakira <fakira.work@gmail.com>
2016-12-03 11:47:16 +0000

    P2 add several kudomon to kudomons list

--- spec/kudomons_spec.rb
+++ spec/kudomons_spec.rb
@@ -2,11 +2,20 @@ describe Kudomons do
   subject(:kudomons) { described_class.new }
 
   describe '#add_kudomon' do
-    let(:mancharred) {double :Kudomon, position: [4,5]}
+    let(:mancharred) {double :Kudomon}
+    let(:sourbulb) {double :Kudomon}
+    let(:chikapu) {double :Kudomon}
 
     it 'can add a kudomon to attribute' do
       kudomons.add_kudomon(mancharred)
       expect(kudomons.avialable_kudomons).to eq [mancharred]
+    end
+
+    it 'can add a kudomon to attribute' do
+      [mancharred, sourbulb, chikapu].each do |kudomon|
+        kudomons.add_kudomon(kudomon)
+      end
+      expect(kudomons.avialable_kudomons).to eq [mancharred, sourbulb, chikapu]
     end
   end
 end
