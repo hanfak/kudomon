1ef2f4ba212a2b343f0d0621fa28c1ec5c378196
Hanif Fakira <fakira.work@gmail.com>
2016-12-03 08:52:55 +0000

    P1 trainer has random position default and changed

--- lib/trainer.rb
+++ lib/trainer.rb
@@ -1,6 +1,16 @@
 class Trainer
-  attr_reader :name
-  def initialize(name)
+  attr_reader :name, :position
+
+  DEFAULT_POSITION_RANGE = [10, 10]
+
+  def initialize(name, max_range = DEFAULT_POSITION_RANGE)
     @name   = name
+    @position  = random_position(max_range)
   end
+
+  private
+    def random_position(max_range)
+      [Kernel.rand(1..max_range[0]),
+       Kernel.rand(1..max_range[1])]
+    end
 end
--- spec/trainer_spec.rb
+++ spec/trainer_spec.rb
@@ -6,6 +6,29 @@ describe Trainer do
       expect(trainer.name).to eq "Han"
     end
 
-    it 'has a random position'
+    describe "#position" do
+      it 'has a position' do
+        allow(Kernel).to receive(:rand).and_return(4, 8)
+        expect(trainer.position).to eq [4,8]
+      end
+
+      it 'position lies between 1 and max range' do
+        max_x_range = 5
+        max_y_range = 9
+        trainer_2 = Trainer.new("Arya" ,  [max_x_range, max_y_range] )
+
+        500.times do
+          expect(trainer_2.position[0]).to be_between(1, max_x_range)
+          expect(trainer_2.position[1]).to be_between(1, max_y_range)
+        end
+      end
+
+      it 'has a default range' do
+        500.times do
+          expect(trainer.position[0]).to be_between(1, 10)
+          expect(trainer.position[1]).to be_between(1, 10)
+        end
+      end
+    end
   end
 end
