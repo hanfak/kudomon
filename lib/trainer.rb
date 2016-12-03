627783edd2e397af82bafcf78aa14e5981e45e52
Hanif Fakira <fakira.work@gmail.com>
2016-12-03 21:48:59 +0000

    P2 extract meth from find_closest_kudomon to private

--- lib/trainer.rb
+++ lib/trainer.rb
@@ -14,15 +14,23 @@ class Trainer
   end
 
   def find_closest_kudomon(kudomons)
-    raise 'No more kudomons available here' if kudomons.available_kudomons.empty?
-
-    @closest_kudomon = kudomons.available_kudomons.min_by do
-      |kudomon|
-      @geospace.find_distance(self, kudomon)
-    end
+    check_availabe_kudomons(kudomons)
+    @closest_kudomon = select_kudomon(kudomons)
   end
 
   def capture_kudomon
     @captured_kudomons << closest_kudomon
   end
+
+  private
+    def select_kudomon(kudomons)
+      kudomons.available_kudomons.min_by do
+        |kudomon|
+        @geospace.find_distance(self, kudomon)
+      end
+    end
+
+    def check_availabe_kudomons(kudomons)
+        raise 'No more kudomons available here' if kudomons.available_kudomons.empty?
+    end
 end
