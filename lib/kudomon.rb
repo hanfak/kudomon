2eab04c5867175c30b22d31b4c29a9f969af38b4
Hanif Fakira <fakira.work@gmail.com>
2016-12-03 08:36:27 +0000

    P1 RF move default range to constant

--- lib/kudomon.rb
+++ lib/kudomon.rb
@@ -1,7 +1,11 @@
 class Kudomon
   attr_reader :species, :type, :position
+  
+  DEFAULT_POSITION_RANGE = [10, 10]
 
-  def initialize(species, type, max_range)
+  def initialize( species,
+                  type,
+                  max_range = DEFAULT_POSITION_RANGE)
     @species        = species
     @type           = type
     @position       = random_position(max_range)
--- spec/kudomon_spec.rb
+++ spec/kudomon_spec.rb
@@ -1,6 +1,6 @@
 describe Kudomon do
   describe "#initialize" do
-    subject(:kudomon) { described_class.new( "Sourbulb" , "grass", [10, 10] ) }
+    subject(:kudomon) { described_class.new( "Sourbulb" , "grass" ) }
 
     it 'has a species' do
       expect(kudomon.species).to eq "Sourbulb"
@@ -26,7 +26,12 @@ describe Kudomon do
         end
       end
 
-      it 'has a default'
+      it 'has a default range' do
+        500.times do
+          expect(kudomon.position[0]).to be_between(1, 10)
+          expect(kudomon.position[1]).to be_between(1, 10)
+        end
+      end
     end
   end
 end
